84797face439e14b784e87db71316440e29600c7
Aftab Farhan <aftabfarhan324@gmail.com>
2025-10-25 23:08:16 +0600

    addes card order

--- src/Components/OderCointner.jsx
+++ src/Components/OderCointner.jsx
@@ -1,6 +1,6 @@
 import React, { use } from "react";
 import CounterBox from "./CounterBox";
-import FullItems from "./Items/FullItems";
+import OrderCart from "./Cart/OrderCart";
 
 const OderCointner = ({ promise }) => {
   const orders = use(promise);
@@ -10,11 +10,18 @@ const OderCointner = ({ promise }) => {
     <div>
       <CounterBox></CounterBox>
       <section className="lexend-font w-11/12 mx-auto py-10">
-         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-            <div className=" col-span-full lg:col-span-7 bg-amber-200">Order</div>
-            <div className="col-span-full lg:col-span-5 bg-red-300" >Cooking</div>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+          <div className=" col-span-full lg:col-span-7 ">
+            <h2 className="text-3xl font-semibold">Current Order</h2>
+            <div className="space-y-9 ">
+              {orders.map((order) => (
+                <OrderCart order={order} key={order.id}></OrderCart>
+              ))}
+            </div>
+          </div>
 
-         </div>
+          <div className="col-span-full lg:col-span-5 bg-red-300">Cooking</div>
+        </div>
       </section>
     </div>
   );
