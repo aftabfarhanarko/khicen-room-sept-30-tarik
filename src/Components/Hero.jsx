09cf665297ea022c6153ab7abc9f492894506b8e
Aftab Farhan <aftabfarhan324@gmail.com>
2025-10-25 20:48:20 +0600

    addes heroo sections

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { ChefHat } from "lucide-react";
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div  className={`bg-[url(/title-bg.png)] flex justify-center items-center gap-1 text-4xl font-bold text-center bg-no-repeat pb-20 pt-10 bg-center`} style={{ backgroundSize: "100% 100%" }}>
+      <h2 className="flex">
+        <ChefHat size={40} />
+        Kitchen Room
+      </h2>
+    </div>
+  );
 };
 
 export default Hero;
